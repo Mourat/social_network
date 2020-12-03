5a0f271d8f6448e96274a9a6ae7fc69b71f407b2
Mourat Boutry <mourat@boutry.me>
2020-12-03 21:14:10 +0100

    03.12.2020

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
 import React from 'react';
+import style from './App.module.scss'
 
 function App() {
   return (
-    <div className="App"></div>
+    <div className={style.app}>
+      <header className={style.header}>HEADER</header>
+      <div className={style.contentArea}>
+        <h1>Hello</h1>
+        <h2>Hello</h2>
+        <h3>Hello</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A aliquid cupiditate ea eveniet obcaecati quae
+          quasi reiciendis, suscipit. Aperiam, aut eius laboriosam nulla quia totam!</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A aliquid cupiditate ea eveniet obcaecati quae
+          quasi reiciendis, suscipit. Aperiam, aut eius laboriosam nulla quia totam!</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A aliquid cupiditate ea eveniet obcaecati quae
+          quasi reiciendis, suscipit. Aperiam, aut eius laboriosam nulla quia totam!</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A aliquid cupiditate ea eveniet obcaecati quae
+          quasi reiciendis, suscipit. Aperiam, aut eius laboriosam nulla quia totam!</p>
+      </div>
+      <footer className={style.footer}>FOOTER</footer>
+    </div>
   );
 }
 
